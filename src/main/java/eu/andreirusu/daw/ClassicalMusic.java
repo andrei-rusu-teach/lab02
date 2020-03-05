59097960c341e40b66ef949e01db4d6dc349050c
Andrei Rusu <andrei.rusu.teach@gmail.com>
2020-03-05 06:35:57 +0200

    Proiectul initializat pentru a efectua lucrarea de lab 02

---
+++ src/main/java/eu/andreirusu/daw/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package eu.andreirusu.daw;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Rapsodia melcului ...";
+    }
+}
---
+++ src/main/java/eu/andreirusu/daw/RockMusic.java
@@ -0,0 +1,8 @@
+package eu.andreirusu.daw;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "ROCK, Rock, rock, ...";
+    }
+}
